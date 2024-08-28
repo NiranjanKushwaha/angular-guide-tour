28305c4e2abcc52e24d6257e0e427a4f65920f55
NiranjanKushwaha <0niranjankumar1@gmail.com>
2024-08-28 11:30:11 +0530

    guided tour code written and it is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from "@angular/core";
 import { RouterOutlet } from '@angular/router';
+import { steps as defaultSteps, defaultStepOptions } from "./config";
+import { ShepherdService } from "angular-shepherd";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers:[ShepherdService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit,AfterViewInit {
   title = 'angular-guided-tour';
+  constructor(private readonly _shepherdService: ShepherdService) {}
+
+  ngOnInit(): void {
+   setTimeout(()=>{
+    this.startTour();
+   },1000)
+  }
+
+  ngAfterViewInit() {
+    this._shepherdService.defaultStepOptions = defaultStepOptions;
+    this._shepherdService.modal = true;
+    this._shepherdService.addSteps(defaultSteps as any);
+  }
+
+  startTour() {
+    this._shepherdService.start();
+  }
 }
